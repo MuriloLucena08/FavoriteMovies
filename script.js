f6079b74670b5246ea09ef0f573f3563f50c4575
MuriloLucena08 <148287052+MuriloLucena08@users.noreply.github.com>
2023-12-04 20:15:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let movieList = JSON.parse(localStorage.getItem('movieList')) ?? [];
 
 async function searchButtonClickHandler() {
   try {
-    let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieNameParameterGenerator()}${movieYearParameterGenerator()}`;
+    let url = `https://www.omdbapi.com/?apikey=${key}&t=${movieNameParameterGenerator()}${movieYearParameterGenerator()}`;
     const response = await fetch(url);
     const data = await response.json();
     createModal(data);
